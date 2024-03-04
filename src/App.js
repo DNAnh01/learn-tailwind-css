bceec577dc9be2977160b72079d44accad3221eb
DNAnh01 <donguyenanhgithub@gmail.com>
2024-03-05 00:02:32 +0700

    feature/router: config router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
-function App() {
-    return <div className="text-xl text-green-800 font-bold">initial and setup ytb app</div>
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/watch/:id" element={<Watch />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
-export default App;
