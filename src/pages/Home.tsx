5ebdd874c5b3f4371ca88f253ce24687df4ae0fb
DNAnh01 <donguyenanhgithub@gmail.com>
2024-03-05 21:43:00 +0700

    feature/Navbar: UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React from 'react';
+import Navbar from '~/components/Navbar';
 
 export default function Home() {
-  return <div>Home</div>;
+    return (
+        <div className="max-h-screen overflow-hidden">
+            <div style={{ height: '7.5vh' }}>
+                <Navbar />
+            </div>
+            <div className="flex" style={{ height: '92.5' }}></div>
+        </div>
+    );
 }
